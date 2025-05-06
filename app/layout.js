7b64ed9c090c5392837d214d1150498724606f2a
Muhammad Zain <rubencallen689@gmail.com>
2025-05-06 11:19:43 +0500

    updated code

--- app/layout.js
+++ app/layout.js
@@ -37,7 +37,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" dir='rtl'>
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
